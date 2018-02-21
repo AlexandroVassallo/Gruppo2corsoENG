bf9097b4697f10cbbca95db035370765c2c67363
Giuseppe <Giuseppe@192.168.23.120>
2018-02-21 18:25:10 +0100

    aggiunto Addres e relative dipendenze in Supplier e User

---
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Address.java
@@ -0,0 +1,55 @@
+package it.eng.projectwork.gruppo02.model;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Embeddable
+public class Address 
+{
+	private String citta;
+	private String via;
+	private String numeroCivico;
+	private String CAP;
+	
+	Address()
+	{
+		
+	}
+		
+	public Address(String citta, String via, String numeroCivico, String CAP) {
+		this.citta=citta;
+		this.via = via;
+		this.numeroCivico = numeroCivico;
+		this.CAP = CAP;
+	}
+	
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getVia() {
+		return via;
+	}
+	public void setVia(String via) {
+		this.via = via;
+	}
+	public String getNumeroCivico() {
+		return numeroCivico;
+	}
+	public void setNumeroCivico(String numeroCivico) {
+		this.numeroCivico = numeroCivico;
+	}
+	public String getCAP() {
+		return CAP;
+	}
+	public void setCAP(String cAP) {
+		CAP = cAP;
+	}
+	
+}
