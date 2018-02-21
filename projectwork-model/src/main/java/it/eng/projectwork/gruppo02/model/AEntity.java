5456909fe19acb4bbf5cce5b51c8a50cdc256707
Alexandro <Alexandro@DESKTOP-TJUAEK4>
2018-02-21 16:32:09 +0100

    AEntity Aggiunta

---
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/AEntity.java
@@ -0,0 +1,19 @@
+package it.eng.projectwork.gruppo02.model;
+
+import java.io.Serializable;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AEntity<PK extends Serializable>{
+	
+	public abstract PK getOid();
+	
+	@Override
+	public String toString() {
+		return "[getOid()="+getOid()+"]";
+	}
+	
+	
+
+}
