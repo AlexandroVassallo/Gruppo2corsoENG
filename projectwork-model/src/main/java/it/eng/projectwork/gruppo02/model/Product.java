dccb3f0b6dc49b9a55d3c677f8b5527a3c9d53b6
Alexandro <Alexandro@DESKTOP-TJUAEK4>
2018-02-22 21:54:49 +0100

    Inserisce Image e Product in model

--- projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Product.java
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Product.java
@@ -18,19 +18,51 @@ public class Product extends AEntity<Long>{
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PRODUCT_ID_SEQ") 
 	@SequenceGenerator(name="PRODUCT_ID_SEQ", sequenceName="PRODUCT_ID_SEQ", allocationSize = 1)
-	private Long id;
+	private Long oid;
 	private String descrizione;
 	
 	@OneToMany(fetch=FetchType.LAZY,mappedBy="product",cascade=CascadeType.ALL)
-	private List<Image> images = new ArrayList<>();
+	private List<Image> images = new ArrayList<Image>();
 	
 	public Product(){
 		
 	}
 	
-	public Product(String description,List<Image> images) {
-		this.descrizione = description;
+	public Product(String descrizione,List<Image> images) {
+		this.descrizione = descrizione;
 		setImages(images);
+}
+	
+	public Long getOid() {
+		return oid;
+	}
+	
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
+	
+	public String getDescrizione() {
+		return descrizione;
+	}
+	
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	public List<Image> getImages() {
+		return images;
+	}
+	
+	public void setImages(List<Image> images) {
+		this.images = images;
+		if(this.images!=null){
+			this.images.forEach((e)->{e.setProduct(this);});
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Supplier [getOid()=" + getOid() + ", getDescription()=" + getDescrizione() + super.toString() + "]";
 }
 	
 }
