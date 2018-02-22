67a4b845f742e55cd38e73adfeba9168906ea2e9
Alexandro <Alexandro@DESKTOP-TJUAEK4>
2018-02-22 20:26:51 +0100

    Image and product

---
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Image.java
@@ -0,0 +1,8 @@
+package it.eng.projectwork.gruppo02.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Image {
+
+}
--- projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Product.java
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo02/model/Product.java
@@ -1,11 +1,36 @@
 package it.eng.projectwork.gruppo02.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
 @Entity
-public class Product {
+public class Product extends AEntity<Long>{
 
 	@Id
-	private String id;
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PRODUCT_ID_SEQ") 
+	@SequenceGenerator(name="PRODUCT_ID_SEQ", sequenceName="PRODUCT_ID_SEQ", allocationSize = 1)
+	private Long id;
+	private String descrizione;
+	
+	@OneToMany(fetch=FetchType.LAZY,mappedBy="product",cascade=CascadeType.ALL)
+	private List<Image> images = new ArrayList<>();
+	
+	public Product(){
+		
+	}
+	
+	public Product(String description,List<Image> images) {
+		this.descrizione = description;
+		setImages(images);
+}
+	
 }
