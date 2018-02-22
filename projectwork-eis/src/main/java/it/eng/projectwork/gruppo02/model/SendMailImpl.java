cf2e9f9c94305a59a6777067005c5de143784dc1
Alexandro <Alexandro@DESKTOP-TJUAEK4>
2018-02-22 16:20:30 +0100

    Alex mod projectwork-ear

--- projectwork-eis/src/main/java/it/eng/projectwork/gruppo02/model/SendMailImpl.java
+++ projectwork-eis/src/main/java/it/eng/projectwork/gruppo02/model/SendMailImpl.java
@@ -2,9 +2,23 @@ package it.eng.projectwork.gruppo02.model;
 
 import javax.ejb.Stateless;
 
+import it.eng.projectwork.gruppo02.model.exception.EmailNotSendException;
+
 @Stateless
 public class SendMailImpl implements SendMail{
-	
 
+	public SendMailImpl() {}
+
+	@Override
+	public void sendMailAllUser(Message message) throws EmailNotSendException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMail(Message message, String destination) throws EmailNotSendException {
+		// TODO Auto-generated method stub
+		
+	};
 
 }
